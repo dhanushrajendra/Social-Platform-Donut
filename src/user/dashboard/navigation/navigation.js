0828612cb28fd6d2c61fb741ee30884fc85ad93d
dhanushrajendra <dhanush.r@shawacademy.com>
2020-01-26 21:17:02 +0530

    basic styling of dashboard

--- src/user/dashboard/navigation/navigation.js
+++ src/user/dashboard/navigation/navigation.js
@@ -5,6 +5,11 @@ import logo from "../../../images/donut.png";
 
 class Navigation extends Component {
   render() {
+    const divStyle = {
+      position: "absolute",
+      bottom: 0
+    };
+
     return (
       <div className="navigation">
         <ListGroup>
@@ -15,6 +20,7 @@ class Navigation extends Component {
           <ListGroup.Item>Pinned Posts</ListGroup.Item>
           <ListGroup.Item>Organization</ListGroup.Item>
           <ListGroup.Item>Account</ListGroup.Item>
+          <ListGroup.Item style={divStyle}>Settings</ListGroup.Item>
         </ListGroup>
       </div>
     );
